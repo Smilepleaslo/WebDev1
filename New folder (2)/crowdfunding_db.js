16e2164df0a4383b64c9c5c1a9f6224bc6ee9e29
Smilepleaslo <suchitstha13@gmail.com>
2024-09-24 13:29:18 +1000

    Correction

--- New folder (2)/crowdfunding_db.js
+++ New folder (2)/crowdfunding_db.js
@@ -1,29 +1,3 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const path=require("path");
-
-const app=express();
-
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(express.static(__dirname));
-
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"Homepage.html"));
-});
-
-app.get("/search",(req,res)=>{
-    res.sendFile(path.join(__dirname,"Searchfundraiser.html"));
-});
-
-app.get("/fundraiser",(req,res)=>{
-    res.sendFile(path.join(__dirname,"Fundraiser.html"));
-});
-
-app.listen(8080, ()=>{
-    console.log("running in 8080");
-});
-
 // setup MySQL connection
 const mysql = require('mysql2');
 
